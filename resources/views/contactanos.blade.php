b2312801f629d684d23270b9d760ead13f25c97e
FernandoFuentes24 <fernandoj2526@gmail.com>
2024-06-20 22:18:55 -0600

    addEndif

--- resources/views/contactanos.blade.php
+++ resources/views/contactanos.blade.php
@@ -13,6 +13,7 @@
             {{ session('success') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
+    @endif
     @ifelse
         <!-- Mostrar mensaje de error si existe -->
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
